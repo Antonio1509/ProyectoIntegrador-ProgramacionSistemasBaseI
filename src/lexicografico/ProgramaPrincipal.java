29b7a8adbe2bfd804157c2bef713e6e44abd5a5c
Antonio1509 <joseassassinscreed15@gmail.com>
2024-11-30 17:45:24 -0600

    Proyecto Analizador Lexico

--- src/lexicografico/ProgramaPrincipal.java
+++ src/lexicografico/ProgramaPrincipal.java
@@ -1,34 +1,41 @@
 package lexicografico;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
 
 public class ProgramaPrincipal {
     public static void main(String[] args) {
-        String filePath = "C:\\Users\\josea\\eclipse-workspace\\ProyectoAnalizadorLexico\\src\\lexicografico\\Palabras"; // Ruta del archivo de texto
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        String filePath = "C:\\Users\\josea\\eclipse-workspace\\ProyectoAnalizadorLexico\\src\\lexicografico\\Palabras"; 
+        String outputFilePath = "C:\\Users\\josea\\eclipse-workspace\\ProyectoAnalizadorLexico\\src\\lexicografico\\Corregido"; 
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+
             String line;
             while ((line = reader.readLine()) != null) {
                 // Crear el analizador léxico con cada línea leída del archivo
                 AcentoLexico lexico = new AcentoLexico(new StringReader(line));
 
-                // Procesar y mostrar el resultado de las palabras con acento
+                // Procesar y escribir las palabras con acento al archivo de salida
                 String resultado;
                 while ((resultado = lexico.yylex()) != null) {
-                    // Ignorar espacios en blanco y solo mostrar palabras procesadas
+                    // Ignorar espacios en blanco y solo escribir palabras procesadas
                     if (!resultado.trim().isEmpty()) {
-                        System.out.print(resultado + " "); // Imprimir palabras con acentos
+                        writer.write(resultado + " "); // Escribir palabras con acentos al archivo
                     }
                 }
-                System.out.println(); // Nueva línea al terminar de procesar la entrada
+                writer.newLine(); // Nueva línea al terminar de procesar la entrada
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
 
 
